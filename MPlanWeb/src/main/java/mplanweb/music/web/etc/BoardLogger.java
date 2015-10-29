ff3bd5857431b1348b4085c22e0ed916b9b7bb7e
mplan <mplanweb@daum.net>
2015-10-29 22:23:41 +0900

    js 수정

--- MPlanWeb/src/main/java/mplanweb/music/web/etc/BoardLogger.java
+++
@@ -1,8 +0,0 @@
-package mplanweb.music.web.etc;
-
-import org.apache.log4j.Logger;
-
-public class BoardLogger {
-	protected static Logger logger = Logger.getLogger("board"); // log4j.xml에서
-																	// 읽어들인다.
-}
